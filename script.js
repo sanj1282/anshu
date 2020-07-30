ad0f4918a66a98a2e4b375b3f3902fcdebfa7ccf
sanj1282 <66018096+sanj1282@users.noreply.github.com>
2020-07-30 21:38:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "anshuman"&&b=="aditi")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
